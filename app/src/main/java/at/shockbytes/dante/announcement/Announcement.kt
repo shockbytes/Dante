d6bd18b4b38f759b41849f494fe23d5f74a429f2
shockbytes <mescht93@gmail.com>
2019-06-24 18:57:36 +0200

    Implement active announcements provider

---
+++ app/src/main/java/at/shockbytes/dante/announcement/Announcement.kt
@@ -0,0 +1,24 @@
+package at.shockbytes.dante.announcement
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.RawRes
+import androidx.annotation.StringRes
+
+data class Announcement (
+    val key: String,
+    @StringRes val titleRes: Int,
+    @StringRes val descriptionRes: Int,
+    val illustration: Illustration?
+) {
+
+    sealed class Illustration {
+
+        data class DrawableIllustration(
+            @DrawableRes val drawableRes: Int
+        ): Illustration()
+
+        data class LottieIllustration(
+            @RawRes val lottieRes: Int
+        ): Illustration()
+    }
+}
---
+++ app/src/main/java/at/shockbytes/dante/announcement/SharedPrefsAnnouncementProvider.kt
@@ -0,0 +1,37 @@
+package at.shockbytes.dante.announcement
+
+import android.content.SharedPreferences
+import at.shockbytes.dante.R
+
+class SharedPrefsAnnouncementProvider(
+    private val sharedPreferences: SharedPreferences
+) : AnnouncementProvider {
+
+
+    private val activeAnnouncement = Announcement(
+        key = "open_source_announcement",
+        titleRes = R.string.announcement_open_source_title,
+        descriptionRes = R.string.announcement_open_source_description,
+        illustration = Announcement.Illustration.LottieIllustration(R.raw.lottie_github)
+    )
+
+    override fun getActiveAnnouncement(): Announcement? {
+
+        val isSeen = isActiveAnnouncementSeen()
+        return if (isSeen) {
+            activeAnnouncement
+        } else {
+            null
+        }
+    }
+
+    private fun isActiveAnnouncementSeen(): Boolean {
+        return sharedPreferences.getBoolean(activeAnnouncement.key, false)
+    }
+
+    override fun markAnnouncementAsSeen(announcement: Announcement) {
+        sharedPreferences.edit()
+            .putBoolean(announcement.key, true)
+            .apply()
+    }
+}

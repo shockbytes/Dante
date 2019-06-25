1ec2b3db82a67b5cabfc8db4e3afebd29babba48
shockbytes <mescht93@gmail.com>
2019-06-25 07:41:48 +0200

    Implement and bind AnnouncementViewModel

--- app/src/main/java/at/shockbytes/dante/ui/fragment/AnnouncementFragment.kt
+++ app/src/main/java/at/shockbytes/dante/ui/fragment/AnnouncementFragment.kt
@@ -1,11 +1,28 @@
 package at.shockbytes.dante.ui.fragment
 
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import at.shockbytes.dante.R
+import at.shockbytes.dante.announcement.Announcement
 import at.shockbytes.dante.dagger.AppComponent
+import at.shockbytes.dante.ui.viewmodel.AnnouncementViewModel
+import javax.inject.Inject
 
 class AnnouncementFragment : BaseFragment() {
 
     override val layoutId: Int = R.layout.fragment_announcement
+
+    @Inject
+    lateinit var vmFactory: ViewModelProvider.Factory
+
+    private lateinit var viewModel: AnnouncementViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(requireActivity(), vmFactory)[AnnouncementViewModel::class.java]
+    }
 
     override fun setupViews() {
         // TODO
@@ -16,12 +33,29 @@ class AnnouncementFragment : BaseFragment() {
     }
 
     override fun bindViewModel() {
+        viewModel.requestAnnouncementState()
+
+        viewModel.getAnnouncementState().observe(this, Observer { announcementState ->
+            when (announcementState) {
+                is AnnouncementViewModel.AnnouncementState.Active -> {
+                    populateAnnouncementViews(announcementState.announcement)
+                }
+                is AnnouncementViewModel.AnnouncementState.Inactive -> {
+                    closeModal()
+                }
+            }
+        })
+    }
+
+    private fun populateAnnouncementViews(announcement: Announcement) {
         // TODO
     }
 
-    override fun unbindViewModel() {
-        // TODO
+    private fun closeModal() {
+        fragmentManager?.popBackStack()
     }
+
+    override fun unbindViewModel() = Unit
 
     companion object {
 
---
+++ app/src/main/java/at/shockbytes/dante/ui/viewmodel/AnnouncementViewModel.kt
@@ -0,0 +1,30 @@
+package at.shockbytes.dante.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import at.shockbytes.dante.announcement.Announcement
+import at.shockbytes.dante.announcement.AnnouncementProvider
+import javax.inject.Inject
+
+class AnnouncementViewModel @Inject constructor(
+    private val announcementProvider: AnnouncementProvider
+) : BaseViewModel() {
+
+    private val announcementState = MutableLiveData<AnnouncementState>()
+    fun getAnnouncementState(): LiveData<AnnouncementState> = announcementState
+
+    fun requestAnnouncementState() {
+        val state = announcementProvider.getActiveAnnouncement()?.let { announcement ->
+            AnnouncementState.Active(announcement)
+        } ?: AnnouncementState.Inactive
+
+        announcementState.postValue(state)
+    }
+
+    sealed class AnnouncementState {
+
+        data class Active(val announcement: Announcement): AnnouncementState()
+
+        object Inactive: AnnouncementState()
+    }
+}

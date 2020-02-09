c7d4acff5ba7957f9b838b6ebdba899c5c5630f7
shockbytes <mescht93@gmail.com>
2020-02-09 16:32:26 +0100

    Remove unused language specific string resources

--- core/src/main/java/at/shockbytes/dante/core/book/Languages.kt
+++ core/src/main/java/at/shockbytes/dante/core/book/Languages.kt
@@ -4,24 +4,35 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import at.shockbytes.dante.core.R
 
-enum class Languages(@StringRes val title: Int, @DrawableRes val image: Int) {
+enum class Languages(
+    @StringRes val title: Int,
+    @DrawableRes val image: Int,
+    val code: String
+) {
 
-    OTHER(R.string.language_other, R.drawable.ic_language_other),
-    ENGLISH(R.string.language_english_full, R.drawable.ic_language_english),
-    GERMAN(R.string.language_german_full, R.drawable.ic_language_german),
-    ITALIAN(R.string.language_italian_full, R.drawable.ic_language_italian),
-    FRENCH(R.string.language_french_full, R.drawable.ic_language_french),
-    SPANISH(R.string.language_spanish_full, R.drawable.ic_language_spanish),
-    PORTUGUESE(R.string.language_portuguese_full, R.drawable.ic_language_portuguese),
-    DUTCH(R.string.language_dutch_full, R.drawable.ic_language_dutch),
-    CHINESE(R.string.language_chinese_full, R.drawable.ic_language_chinese),
-    RUSSIAN(R.string.language_russian_full, R.drawable.ic_language_russian),
-    SWEDISH(R.string.language_swedish_full, R.drawable.ic_language_swedish),
-    NORWEGIAN(R.string.language_norwegian_full, R.drawable.ic_language_norwegian),
-    POLISH(R.string.language_polish_full, R.drawable.ic_language_polish),
-    RUMANIAN(R.string.language_romanian_full, R.drawable.ic_language_romanian),
-    CROATIAN(R.string.language_croatian_full, R.drawable.ic_language_croatian),
-    HUNGARIAN(R.string.language_hungary_full, R.drawable.ic_language_hungary),
-    INDONESIAN(R.string.language_indonesian_full, R.drawable.ic_language_indonesian),
-    THAI(R.string.language_thai_full, R.drawable.ic_language_thai)
+    OTHER(R.string.language_other, R.drawable.ic_language_other, "na"),
+    ENGLISH(R.string.language_english_full, R.drawable.ic_language_english, "en"),
+    GERMAN(R.string.language_german_full, R.drawable.ic_language_german, "de"),
+    ITALIAN(R.string.language_italian_full, R.drawable.ic_language_italian, "it"),
+    FRENCH(R.string.language_french_full, R.drawable.ic_language_french, "fr"),
+    SPANISH(R.string.language_spanish_full, R.drawable.ic_language_spanish, "es"),
+    PORTUGUESE(R.string.language_portuguese_full, R.drawable.ic_language_portuguese, "pt"),
+    DUTCH(R.string.language_dutch_full, R.drawable.ic_language_dutch, "nl"),
+    CHINESE(R.string.language_chinese_full, R.drawable.ic_language_chinese, "cn"),
+    RUSSIAN(R.string.language_russian_full, R.drawable.ic_language_russian, "ru"),
+    SWEDISH(R.string.language_swedish_full, R.drawable.ic_language_swedish, "se"),
+    NORWEGIAN(R.string.language_norwegian_full, R.drawable.ic_language_norwegian, "no"),
+    POLISH(R.string.language_polish_full, R.drawable.ic_language_polish, "pl"),
+    RUMANIAN(R.string.language_romanian_full, R.drawable.ic_language_romanian, "ro"),
+    CROATIAN(R.string.language_croatian_full, R.drawable.ic_language_croatian, "hr"),
+    HUNGARIAN(R.string.language_hungary_full, R.drawable.ic_language_hungary, "hu"),
+    INDONESIAN(R.string.language_indonesian_full, R.drawable.ic_language_indonesian, "id"),
+    THAI(R.string.language_thai_full, R.drawable.ic_language_thai, "th");
+
+    companion object {
+
+        fun fromLanguageCode(code: String): Languages {
+            return values().find { it.code == code } ?: OTHER
+        }
+    }
 }

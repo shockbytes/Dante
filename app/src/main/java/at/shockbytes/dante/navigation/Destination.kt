27c6119f1df29901e399b3799427a4466cbc9885
shockbytes <mescht93@gmail.com>
2020-02-20 07:14:21 +0100

    Improve ActivityNavigator setup

--- app/src/main/java/at/shockbytes/dante/navigation/Destination.kt
+++ app/src/main/java/at/shockbytes/dante/navigation/Destination.kt
@@ -1,35 +1,113 @@
 package at.shockbytes.dante.navigation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Parcelable
+import at.shockbytes.dante.R
+import at.shockbytes.dante.camera.BarcodeCaptureActivity
 import at.shockbytes.dante.core.book.BookEntity
+import at.shockbytes.dante.core.createSharingIntent
+import at.shockbytes.dante.ui.activity.BackupActivity
+import at.shockbytes.dante.ui.activity.DetailActivity
+import at.shockbytes.dante.ui.activity.MainActivity
+import at.shockbytes.dante.ui.activity.ManualAddActivity
+import at.shockbytes.dante.ui.activity.NotesActivity
+import at.shockbytes.dante.ui.activity.SearchActivity
+import at.shockbytes.dante.ui.activity.SettingsActivity
+import at.shockbytes.dante.ui.activity.StatisticsActivity
+import at.shockbytes.dante.ui.activity.TimeLineActivity
 import kotlinx.android.parcel.Parcelize
 
 sealed class Destination {
 
-    data class BookDetail(val info: BookDetailInfo) : Destination() {
+    abstract fun provideIntent(context: Context): Intent
+
+    data class BookDetail(private val info: BookDetailInfo) : Destination() {
 
         @Parcelize
         data class BookDetailInfo(
             val id: Long,
             val title: String
         ) : Parcelable
+
+        override fun provideIntent(context: Context): Intent {
+            return DetailActivity.newIntent(context, info.id, info.title)
+        }
     }
 
-    data class Share(val bookEntity: BookEntity) : Destination()
+    data class Share(private val bookEntity: BookEntity) : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return Intent.createChooser(
+                bookEntity.createSharingIntent(context),
+                context.resources.getText(R.string.send_to)
+            )
+        }
+    }
 
     data class Main(
-        val bookDetailInfo: BookDetail.BookDetailInfo? = null,
-        val openCameraAfterLaunch: Boolean = false
-    ) : Destination()
+        private val bookDetailInfo: BookDetail.BookDetailInfo? = null,
+        private val openCameraAfterLaunch: Boolean = false
+    ) : Destination() {
 
-    object Search : Destination()
-    data class ManualAdd(val updatedBookEntity: BookEntity? = null) : Destination()
-    object Statistics : Destination()
-    object Timeline : Destination()
-    object Backup : Destination()
-    object Settings : Destination()
+        override fun provideIntent(context: Context): Intent {
+            return MainActivity.newIntent(context, bookDetailInfo, openCameraAfterLaunch)
+        }
+    }
 
-    object BarcodeScanner : Destination()
+    object Search : Destination() {
 
-    data class Notes(val notesBundle: NotesBundle) : Destination()
+        override fun provideIntent(context: Context): Intent {
+            return SearchActivity.newIntent(context)
+        }
+    }
+
+    data class ManualAdd(private val updatedBookEntity: BookEntity? = null) : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return ManualAddActivity.newIntent(context, updatedBookEntity)
+        }
+    }
+
+    object Statistics : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return StatisticsActivity.newIntent(context)
+        }
+    }
+
+    object Timeline : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return TimeLineActivity.newIntent(context)
+        }
+    }
+
+    object Backup : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return BackupActivity.newIntent(context)
+        }
+    }
+
+    object Settings : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return SettingsActivity.newIntent(context)
+        }
+    }
+
+    object BarcodeScanner : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return BarcodeCaptureActivity.newIntent(context)
+        }
+    }
+
+    data class Notes(private val notesBundle: NotesBundle) : Destination() {
+
+        override fun provideIntent(context: Context): Intent {
+            return  NotesActivity.newIntent(context, notesBundle)
+        }
+    }
 }

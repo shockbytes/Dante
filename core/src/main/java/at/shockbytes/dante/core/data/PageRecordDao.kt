3bf173d1f6811918e3a9a07d134a506b32f3c05c
shockbytes <mescht93@gmail.com>
2020-08-10 21:34:51 +0200

    Implement function RealmPageRecordDao::insertPageRecordForId

--- core/src/main/java/at/shockbytes/dante/core/data/PageRecordDao.kt
+++ core/src/main/java/at/shockbytes/dante/core/data/PageRecordDao.kt
@@ -1,9 +1,17 @@
 package at.shockbytes.dante.core.data
 
 import at.shockbytes.dante.core.book.realm.PageRecord
+import io.reactivex.Completable
 import io.reactivex.Observable
 
 interface PageRecordDao {
+
+    fun insertPageRecordForId(
+            id: Long,
+            fromPage: Int,
+            toPage: Int,
+            nowInMillis: Long
+    ): Completable
 
     fun pageRecordsForBook(bookId: Long): Observable<List<PageRecord>>
 }
--- core/src/main/java/at/shockbytes/dante/core/data/local/RealmPageRecordDao.kt
+++ core/src/main/java/at/shockbytes/dante/core/data/local/RealmPageRecordDao.kt
@@ -4,6 +4,7 @@ import at.shockbytes.dante.core.book.realm.PageRecord
 import at.shockbytes.dante.core.book.realm.RealmInstanceProvider
 import at.shockbytes.dante.core.book.realm.RealmPageRecord
 import at.shockbytes.dante.core.data.PageRecordDao
+import io.reactivex.Completable
 import io.reactivex.Observable
 import io.realm.Sort
 
@@ -12,6 +13,37 @@ class RealmPageRecordDao(private val realm: RealmInstanceProvider) : PageRecordD
     private val mapper = RealmPageRecordMapper()
 
     private val pageRecordClass = RealmPageRecord::class.java
+
+    override fun insertPageRecordForId(
+            id: Long,
+            fromPage: Int,
+            toPage: Int,
+            nowInMillis: Long
+    ): Completable {
+        return Completable.fromAction {
+
+            val date = formatDate(nowInMillis)
+
+            insert(
+                    PageRecord(
+                            bookId = id,
+                            fromPage = fromPage,
+                            toPage = toPage,
+                            date = date
+                    )
+            )
+        }
+    }
+
+    private fun formatDate(nowInMillis: Long): String {
+        return "TODO"
+    }
+
+    private fun insert(pageRecord: PageRecord) {
+        realm.instance.executeTransaction { realm ->
+            realm.copyToRealm(mapper.mapFrom(pageRecord))
+        }
+    }
 
     override fun pageRecordsForBook(bookId: Long): Observable<List<PageRecord>> {
         return realm.instance.where(pageRecordClass)

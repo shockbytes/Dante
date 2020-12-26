952ed13b1cacfbd6b91fdc322e56e5f01da15509
shockbytes <mescht93@gmail.com>
2020-12-26 13:19:40 +0100

    Nice UI for search items

--- app/src/main/java/at/shockbytes/dante/ui/custom/bookspages/BooksAndPagesDiagramView.kt
+++ app/src/main/java/at/shockbytes/dante/ui/custom/bookspages/BooksAndPagesDiagramView.kt
@@ -5,7 +5,6 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
 import at.shockbytes.dante.R
 import at.shockbytes.dante.ui.custom.DanteMarkerView
 import at.shockbytes.dante.util.getThemeFont

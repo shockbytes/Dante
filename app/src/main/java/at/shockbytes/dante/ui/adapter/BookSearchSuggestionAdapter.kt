000566dced70f50f81842b488aadc202877d0621
shockbytes <mescht93@gmail.com>
2020-12-26 19:45:35 +0100

    Remove kotterknife dependency in BookSearchSuggestionAdapter

--- app/src/main/java/at/shockbytes/dante/ui/adapter/BookSearchSuggestionAdapter.kt
+++ app/src/main/java/at/shockbytes/dante/ui/adapter/BookSearchSuggestionAdapter.kt
@@ -3,15 +3,12 @@ package at.shockbytes.dante.ui.adapter
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import at.shockbytes.dante.R
 import at.shockbytes.dante.core.book.BookSearchItem
 import at.shockbytes.dante.core.image.ImageLoader
 import at.shockbytes.util.adapter.BaseAdapter
 import kotlinx.android.extensions.LayoutContainer
-import kotterknife.bindView
+import kotlinx.android.synthetic.main.item_book_search_suggestion.*
 
 /**
  * Author: Martin Macheiner
@@ -32,18 +29,15 @@ class BookSearchSuggestionAdapter(
         override val containerView: View
     ) : BaseAdapter.ViewHolder<BookSearchItem>(containerView), LayoutContainer {
 
-        private val txtTitle: TextView by bindView(R.id.item_book_search_suggestion_txt_title)
-        private val txtAuthor: TextView by bindView(R.id.item_book_search_suggestion_txt_author)
-        private val btnAdd: Button by bindView(R.id.item_book_search_suggestion_btn_add)
-        private val imgViewCover: ImageView by bindView(R.id.item_book_search_suggestion_imgview_cover)
-
         override fun bindToView(content: BookSearchItem, position: Int) {
-            txtTitle.text = content.title
-            txtAuthor.text = content.author
+            item_book_search_suggestion_txt_title.text = content.title
+            item_book_search_suggestion_txt_author.text = content.author
 
-            btnAdd.visibility = if (content.bookId < 0) View.VISIBLE else View.GONE
-            btnAdd.setOnClickListener {
-                addClickedListener.invoke(content)
+            item_book_search_suggestion_btn_add.apply {
+                visibility = if (content.bookId < 0) View.VISIBLE else View.GONE
+                setOnClickListener {
+                    addClickedListener.invoke(content)
+                }
             }
 
             loadImage(content.thumbnailAddress)
@@ -54,7 +48,7 @@ class BookSearchSuggestionAdapter(
                 imageLoader.loadImageWithCornerRadius(
                     context,
                     thumbnailAddress,
-                    imgViewCover,
+                    item_book_search_suggestion_imgview_cover,
                     R.drawable.ic_placeholder,
                     cornerDimension = context.resources.getDimension(R.dimen.thumbnail_rounded_corner).toInt()
                 )

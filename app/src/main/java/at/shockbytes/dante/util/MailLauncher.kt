fa7faaddb7640a29225df05c9691b9e822793d5b
shockbytes <mescht93@gmail.com>
2021-01-10 18:45:16 +0100

    Add problem button to Login screen and let users send mail when they have login problems

--- app/src/main/java/at/shockbytes/dante/util/MailLauncher.kt
+++ app/src/main/java/at/shockbytes/dante/util/MailLauncher.kt
@@ -2,6 +2,7 @@ package at.shockbytes.dante.util
 
 import android.app.Activity
 import androidx.core.app.ShareCompat
+import at.shockbytes.dante.BuildConfig
 import at.shockbytes.dante.R
 
 object MailLauncher {
@@ -12,8 +13,13 @@ object MailLauncher {
         activity: Activity,
         subject: String,
         body: String = "",
-        recipient: String = SHOCKBYTES_STUDIO_MAIL
+        recipient: String = SHOCKBYTES_STUDIO_MAIL,
+        attachVersion: Boolean = false
     ) {
+
+        val formattedBody = if (attachVersion) {
+            "$body\n\n\n${getVersionDetails()}"
+        } else body
 
         ShareCompat.IntentBuilder.from(activity)
             .setType("message/rfc822")
@@ -22,5 +28,9 @@ object MailLauncher {
             .setText(body)
             .setChooserTitle(activity.getString(R.string.action_send_mail))
             .startChooser()
+    }
+
+    private fun getVersionDetails(): String {
+        return "Version ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
     }
 }
